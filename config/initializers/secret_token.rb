65111e6dd3e185dfb4f6ef260387dcff139d1dc4
Adrian Mugnolo <adrian@wyeworks.com>
2013-10-25 08:20:57 -0200

    Create application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weather::Application.config.secret_key_base = '1939a4b2dcd8d93da5f18adcd90754f96690130dea68bab7373e1f722b991d6e9967d75adabf7d195b732825a3ce4f345f5df19be9a908e6dc2b8434cc33064a'
