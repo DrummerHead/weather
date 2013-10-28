73b8c6afd2887bb1e299087a891353b1f8371f90
Adrian Mugnolo <adrian@wyeworks.com>
2013-10-28 15:31:19 -0200

    Nest resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Weather::Application.routes.draw do
-  resources :cities
-
-  resource :condition
+  resources :cities do
+    resource :condition
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'conditions#show'
+  root 'cities#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- test/controllers/conditions_controller_test.rb
+++ test/controllers/conditions_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ConditionsControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, city_id: 123
     assert_response :success
     assert_not_nil assigns(:condition)
   end
