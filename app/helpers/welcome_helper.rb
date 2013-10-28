62567436a4d639b7b2c20b2dde0baee911fee96a
Adrian Mugnolo <adrian@wyeworks.com>
2013-10-28 10:44:19 -0200

    Move to helper

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,5 @@
 module WelcomeHelper
+  def format_temperature(value)
+    '%.1f' % value
+  end
 end
--- test/helpers/welcome_helper_test.rb
+++ test/helpers/welcome_helper_test.rb
@@ -1,4 +1,15 @@
 require 'test_helper'
 
 class WelcomeHelperTest < ActionView::TestCase
+  test "temperature format for an integer value" do
+    assert_equal '15.0', format_temperature(15)
+  end
+
+  test "temperature format for a negative value" do
+    assert_equal '-15.0', format_temperature(-15)
+  end
+
+  test "temperature format for a decimal value" do
+    assert_equal '12.3', format_temperature(12.345)
+  end
 end
