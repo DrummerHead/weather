56cef7afc583d2804dd98b5a51402db6538aec3d
Adrian Mugnolo <adrian@wyeworks.com>
2013-10-28 15:35:05 -0200

    Load parent resource

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,5 +1,6 @@
 class ConditionsController < ApplicationController
   def show
+    @city = City.find(params[:city_id])
     @condition = Condition.new
   end
 end
--- test/controllers/conditions_controller_test.rb
+++ test/controllers/conditions_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class ConditionsControllerTest < ActionController::TestCase
+  setup do
+    @city = cities(:montevideo)
+  end
+
   test "should get show" do
-    get :show, city_id: 123
+    get :show, city_id: @city
     assert_response :success
+    assert_equal @city, assigns(:city)
     assert_not_nil assigns(:condition)
   end
 end
